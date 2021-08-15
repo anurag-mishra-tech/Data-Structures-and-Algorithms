d0b4d4a60ae73a4fcafbd8e4914e2a27be52ae6e
ANURAG MISHRA <anurag.mishra3@dxc.com>
2021-08-15 22:00:01 +0530

    Update

---
+++ src/com/dsa/arrays/SortArrays.java
@@ -0,0 +1,21 @@
+package com.dsa.arrays;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SortArrays {
+	public static void main(String[] args) {
+		int[] intArray = new int[7];
+		intArray[0] = 20;
+		intArray[1] = 30;
+		intArray[2] = 40;
+		intArray[3] = 50;
+		intArray[4] = 60;
+		intArray[5] = 70;
+		intArray[6] = 80;
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		
+	}
+}
